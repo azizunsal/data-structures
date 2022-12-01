ff62c3d5641a00674aac32f5569f7500e269eefa
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-12-01 22:50:06 +0300

    Modified array based stack impl using resizing array on push on pop.

--- stack/src/main/java/com/aunsal/ds/stack/impl/ArrayStack.java
+++ stack/src/main/java/com/aunsal/ds/stack/impl/ArrayStack.java
@@ -7,27 +7,50 @@ import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class ArrayStack<T> implements Stack<T> {
-    private static final int INITIAL_CAPACITY = 10;
+    private static final int INITIAL_CAPACITY = 1;
     private T[] values;
     private int size;
+    private Class clazz;
 
     public ArrayStack(Class clazz) {
         this.values = (T[]) Array.newInstance(clazz, INITIAL_CAPACITY);
+        this.clazz = clazz;
     }
+
+    private void checkCapacityOnPush() {
+        if (size < values.length) return;
+        resize(values.length * 2);
+    }
+
+    private void checkCapacityOnPop() {
+        if (size == 0) return;
+        if (size != values.length / 4) return;
+        resize(values.length / 2);
+    }
+
+    private void resize(int capacity) {
+        T[] newArr = (T[]) Array.newInstance(clazz, capacity);
+        for (int i = 0; i < size; i++) {
+            newArr[i] = values[i];
+        }
+        values = newArr;
+        System.out.println("resized=" + Arrays.toString(values));
+    }
+
 
     @Override
     public void push(T data) {
-        values[size] = data;
-        size++;
+        checkCapacityOnPush();
+        values[size++] = data;
     }
 
     @Override
     public T pop() throws EmptyStackException {
         if (isEmpty()) throw new EmptyStackException();
-        T current = values[size - 1];
+        T current = values[--size];
         // Avoid loitering!
-        values[size - 1] = null;
-        size--;
+        values[size] = null;
+        checkCapacityOnPop();
         return current;
     }
 
