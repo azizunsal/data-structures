80dbe40f26d04dab4fec3d1a2a2a2d400a9dcead
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-12-01 16:55:20 +0300

    Implemented another stack data structure using a fixed size array.

--- stack/src/main/java/com/aunsal/ds/stack/client/StackClient.java
+++ stack/src/main/java/com/aunsal/ds/stack/client/StackClient.java
@@ -1,6 +1,7 @@
 package com.aunsal.ds.stack.client;
 
 import com.aunsal.ds.stack.Stack;
+import com.aunsal.ds.stack.impl.ArrayStack;
 import com.aunsal.ds.stack.impl.LinkedStack;
 
 import java.util.Scanner;
@@ -9,7 +10,11 @@ public class StackClient {
     public static String EXIT_IDENTIFIER = "exit";
 
     public static void main(String[] args) {
-        Stack<String> stack = new LinkedStack<String>();
+//        Stack<String> stack = new LinkedStack<String>();
+        Stack<String> stack = new ArrayStack<>(String.class);
+        boolean isStackEmpty = stack.isEmpty();
+        System.out.println("Is the stack empty=" + isStackEmpty);
+
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String input = scanner.next();
---
+++ stack/src/main/java/com/aunsal/ds/stack/impl/ArrayStack.java
@@ -0,0 +1,37 @@
+package com.aunsal.ds.stack.impl;
+
+import com.aunsal.ds.stack.Stack;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class ArrayStack<T> implements Stack<T> {
+    private static final int INITIAL_CAPACITY = 10;
+    private T[] values;
+    private int size;
+
+    public ArrayStack(Class clazz) {
+        this.values = (T[]) Array.newInstance(clazz, INITIAL_CAPACITY);
+    }
+
+    @Override
+    public void push(T data) {
+        values[size] = data;
+        size++;
+    }
+
+    @Override
+    public T pop() throws EmptyStackException {
+        if (isEmpty()) throw new EmptyStackException();
+        T current = values[size - 1];
+//        values[size] = null;
+        size--;
+        return current;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
